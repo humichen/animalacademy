2dd0edb2ebbe074fc948cdc106340c3ea3cd8b01
humichen <43415952+humichen@users.noreply.github.com>
2019-11-03 22:01:18 +0800

    Add files via upload

---
+++ js/jQuery.js
@@ -0,0 +1,14 @@
+$(function(){
+      $('.nav-list-item1').click(function(){
+        $(this).css('border','5px #A77126 dotted').css('width','90px').css('height','77px').css('background-size','80px 67px').css('margin-left','30px');
+      });  
+      $('.nav-list-item2').click(function(){
+        $(this).css('border','5px #A77126 dotted').css('width','90px').css('height','77px').css('background-size','80px 67px').css('margin-left','30px');
+      }); 
+      $('.nav-list-item3').click(function(){
+        $(this).css('border','5px #A77126 dotted').css('width','90px').css('height','77px').css('background-size','80px 67px').css('margin-left','30px');
+      }); 
+      $('.nav-list-item4').click(function(){
+        $(this).css('border','5px #A77126 dotted').css('width','90px').css('height','77px').css('background-size','80px 67px').css('margin-left','30px');
+      }); 
+});
