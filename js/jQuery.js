6c61674a1b99d79387330890391394d351fe8383
humichen <43415952+humichen@users.noreply.github.com>
2019-11-06 19:19:12 +0800

    Add files via upload

--- js/jQuery.js
+++ js/jQuery.js
@@ -11,4 +11,17 @@ $(function(){
       $('.nav-list-item4').click(function(){
         $(this).css('border','5px #A77126 dotted').css('width','90px').css('height','77px').css('background-size','80px 67px').css('margin-left','30px');
       }); 
+      $("#gotop").click(function(){
+        jQuery("html,body").animate({
+            scrollTop:0
+        },333);
+      });
+      $(window).scroll(function() {
+        if ( $(this).scrollTop() > 300 ){
+          $('#gotop').fadeIn(222);
+        } else {
+          $('#gotop').stop().fadeOut(222);
+        }
+      }).scroll();
+      
 });
